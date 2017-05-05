c838eb4c25ddf57b1b8c1773d7dc5762b2ecc905
Ahmad Dimas Abid M <ahmaddimas0@gmail.com>
2017-05-05 14:20:58 +0700

    setting config

---
+++ application/models/m_admin.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class M_Admin extends CI_Model {
+    public function __construct()
+    {
+        parent::__construct();
+        //Do your magic here
+    }
+    
+}
+/* End of file ${TM_FILENAME:${1/(.+)/lMAadmin.php/}} */
+/* Location: ./${TM_FILEPATH/.+((?:application).+)/M_Admin/:application/models/${1/(.+)/lMAadmin.php/}} */
