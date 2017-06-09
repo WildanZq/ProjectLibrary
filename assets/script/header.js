1db4e61380baacb39e4da381c4c73ac5dbc76721
WildanZq <wildan2wildan@gmail.com>
2017-06-09 22:23:36 +0700

    fix header.js

--- assets/script/header.js
+++ assets/script/header.js
@@ -1,35 +1,19 @@
 function adaptHeader() {
   if($(document).scrollTop() <= 100) {
-      $('.header').css({
-          "background": "none", "box-shadow": "none"
-      });
-//            $('.header *').css({
-//                "color": "white"
-//            });
       $('.header img').css({
           "transform": "translateY(-100%)"
       });
-//            spanAfter.innerHTML = spanAfter.innerHTML.replace(spanAfter.innerHTML.substr(20, 19), "border-color: white");
   } else {
-      $('.header').css({
-          "background-color": "#d2232a","box-shadow": "0 0 10px black"
-      });
-//            $('.header *').css({
-//                "color": "black"
-//            });
-      $('.header img').css({
-          "transform": "translateY(0)"
-      });
-//            $('.header span').css({
-//                "color": "rgba(0, 0, 0, 0.8)"
-//            });
-//            spanAfter.innerHTML = spanAfter.innerHTML.replace(spanAfter.innerHTML.substr(20, 19), "border-color: black");
+    $('.header img').css({
+        "transform": "translateY(0)"
+    });
   }
+      var op = $(document).scrollTop()/125;
+      $('.header').css({
+          "background-color": "rgba(210, 35, 42, "+op+")","box-shadow": "0 0 10px rgba(0,0,0,"+op+")"
+      });
 }
 $(document).ready(function(){
-//    var spanAfter = document.createElement("style");
-//    spanAfter.innerHTML = ".header span:after {border-color: white}";
-//    document.head.appendChild(spanAfter);
     adaptHeader();
     $(window).scroll(function() {
         adaptHeader();
